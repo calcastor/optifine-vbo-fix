52297490ff19157d3695b08f8273a5f8fa52d1cc
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2022-08-08 11:25:04 -0700

    Add lockup fix
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

---
+++ src/main/java/dev/calcastor/vbofix/mixins/VertexBufferMixin.java
@@ -0,0 +1,23 @@
+package dev.calcastor.vbofix.mixins;
+
+import net.minecraft.client.renderer.vertex.VertexBuffer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.nio.ByteBuffer;
+
+@Mixin(VertexBuffer.class)
+public abstract class VertexBufferMixin {
+    @Shadow private int glBufferId;
+
+    // inject at INVOKE rather than HEAD as OptiFine changes this method for its Render Regions option
+    @Inject(method = "bufferData", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/vertex/VertexBuffer;bindBuffer()V"), cancellable = true)
+    private void ignoreRemovedBuffers(ByteBuffer byteBuffer, CallbackInfo ci) {
+        if (this.glBufferId == -1) {
+            ci.cancel();
+        }
+    }
+}
